58f117d10f90aa6daa7d1a383664978faf7342ec
Ren Guidolin <8.renji@gmail.com>
2022-01-20 02:44:26 -0500

    removing refactor from daylist and daylist item because they stopped the functions from working

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,28 +7,38 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  const reset = () => {
+    setInterviewer(null)
+    setStudent('')
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            name={student}
+            name="name"
             type="text"
+            value={student}
             placeholder="Enter Student Name"
-            onChange={(event) => setStudent(event.target.value)}
+            onChange={(event) => setStudent(event.target.value)} //got from hello react
           />
         </form>
         <InterviewerList
-          name={interviewer}
-          type="text" 
+          value={interviewer}
           interviewers={props.interviewers}
-          onClick={(event) => setInterviewer(event.target.value)}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
