398f2aed1c0a9e0011b3bd8791397049595a1901
Ren Guidolin <8.renji@gmail.com>
2022-01-21 01:14:05 -0500

    finished for day 4

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,46 +3,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList"
 import Appointment from "./Appointment";
-
-
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer:{
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer:{
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       }
-//     }
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//   }
-// ];
+import { getAppointmentsForDay } from "Helpers/selectors";
 
 
 
@@ -53,15 +14,11 @@ export default function Application(props) {
     appointments: {}
   });
 
-  const dailyAppointments = [];
+  const dailyAppointments = getAppointmentsForDay(state, state.day)
   
   const setDay = day => setState(
     { ...state, day }
   );
-
-  const setDays = (days) => {
-    setState(prev => ({ ...prev, days }));
-  };
 
   const apts = dailyAppointments.map((apt) => {
     return (
@@ -70,9 +27,12 @@ export default function Application(props) {
   })
 
   useEffect(() => {
-    const daysURL = `http://localhost:8001/api/days`
-    axios.get(daysURL).then (response => {
-      setDays(response.data)
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments')
+    ]).then((all) => {
+      const [first, second] = all;
+      setState(prev => ({...prev, days: first.data, appointments: second.data}))
     })
   }, [])
 
